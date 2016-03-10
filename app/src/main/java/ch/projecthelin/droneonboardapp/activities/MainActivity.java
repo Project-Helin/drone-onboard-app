4df930a79ad90aeeda9637ae3f85fd9f9964164a
Marcel Amsler <marcel.amsler@gmail.com>
2016-03-10 17:19:28 +0100

    removed fragmentinteraction listener

--- app/src/main/java/ch/projecthelin/droneonboardapp/activities/MainActivity.java
+++ app/src/main/java/ch/projecthelin/droneonboardapp/activities/MainActivity.java
@@ -1,23 +1,18 @@
 package ch.projecthelin.droneonboardapp.activities;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 import ch.projecthelin.droneonboardapp.R;
 import ch.projecthelin.droneonboardapp.fragments.MainFragment;
 import ch.projecthelin.droneonboardapp.fragments.PlaceholderFragment;
@@ -26,9 +21,6 @@ public class MainActivity extends AppCompatActivity {
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ViewPager mViewPager;
 
     @Override
