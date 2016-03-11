8574f54c2a9add0f673a86d18a20c75d26aa2bee
Marcel Amsler <marcel.amsler@gmail.com>
2016-03-11 12:08:13 +0100

    removed floating button

---
+++ app/src/main/java/ch/projecthelin/droneonboardapp/activities/MissionActivity.java
@@ -0,0 +1,17 @@
+package ch.projecthelin.droneonboardapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import ch.projecthelin.droneonboardapp.R;
+
+public class MissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission);
+    }
+
+
+
+}
