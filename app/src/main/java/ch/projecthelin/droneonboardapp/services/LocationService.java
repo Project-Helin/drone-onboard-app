670503fd0b2a2263cd053cfb1363e3470f9c4170
Marcel Amsler <marcel.amsler@gmail.com>
2016-05-04 14:25:00 +0200

    refactored

--- app/src/main/java/ch/projecthelin/droneonboardapp/services/LocationService.java
+++ app/src/main/java/ch/projecthelin/droneonboardapp/services/LocationService.java
@@ -5,11 +5,9 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.widget.Toast;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
@@ -18,11 +16,10 @@ import com.google.android.gms.location.LocationServices;
 import javax.inject.Inject;
 
 
-public class LocationService implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+public class LocationService implements GoogleApiClient.ConnectionCallbacks, LocationListener {
 
     private Context context;
     private GoogleApiClient mGoogleApiClient;
-    private Location currentLocation;
     private LocationRequest mLocationRequest;
     private LocationListener locationListener;
     private static final int UPDATE_DISTANCE_IN_METERS = 5;
@@ -38,12 +35,11 @@ public class LocationService implements GoogleApiClient.ConnectionCallbacks, Goo
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(context)
                     .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
                     .build();
 
-            mGoogleApiClient.connect();
         }
+        mGoogleApiClient.connect();
 
         mLocationRequest = LocationRequest.create()
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
@@ -76,13 +72,7 @@ public class LocationService implements GoogleApiClient.ConnectionCallbacks, Goo
     }
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
     public void onLocationChanged(Location updatedLocation) {
-        currentLocation = updatedLocation;
         locationListener.onLocationChanged(updatedLocation);
     }
 
