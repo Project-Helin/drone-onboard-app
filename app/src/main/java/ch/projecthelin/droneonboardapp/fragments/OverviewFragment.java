02a3a71b8cf1420951aecf26dea49312d0fa439b
Marcel Amsler <marcel.amsler@gmail.com>
2016-05-29 14:59:55 +0200

    removed test mission button

--- app/src/main/java/ch/projecthelin/droneonboardapp/fragments/OverviewFragment.java
+++ app/src/main/java/ch/projecthelin/droneonboardapp/fragments/OverviewFragment.java
@@ -7,33 +7,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-
-import ch.helin.messages.dto.Action;
-import ch.helin.messages.dto.MissionDto;
-import ch.helin.messages.dto.OrderProductDto;
-import ch.helin.messages.dto.ProductDto;
-import ch.helin.messages.dto.message.missionMessage.FinalAssignMissionMessage;
 import ch.helin.messages.dto.state.BatteryState;
 import ch.helin.messages.dto.state.DroneState;
-
 import ch.helin.messages.dto.state.GpsQuality;
 import ch.helin.messages.dto.state.GpsState;
-import ch.helin.messages.dto.way.Position;
-import ch.helin.messages.dto.way.RouteDto;
-import ch.helin.messages.dto.way.Waypoint;
 import ch.projecthelin.droneonboardapp.DroneOnboardApp;
-import ch.projecthelin.droneonboardapp.listeners.MessagingConnectionListener;
 import ch.projecthelin.droneonboardapp.R;
-
-
-import ch.projecthelin.droneonboardapp.activities.MainActivity;
 import ch.projecthelin.droneonboardapp.listeners.DroneConnectionListener;
+import ch.projecthelin.droneonboardapp.listeners.MessagingConnectionListener;
 import ch.projecthelin.droneonboardapp.services.DroneConnectionService;
 import ch.projecthelin.droneonboardapp.services.MessagingConnectionService;
 import com.o3dr.android.client.apis.drone.ExperimentalApi;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
 
 
 public class OverviewFragment extends Fragment implements DroneConnectionListener, MessagingConnectionListener {
@@ -88,7 +74,6 @@ public class OverviewFragment extends Fragment implements DroneConnectionListene
         txtGPS = (TextView) view.findViewById(R.id.txtGPS);
         txtBattery = (TextView) view.findViewById(R.id.txtBattery);
         txtServerConnectionState = (TextView) view.findViewById(R.id.server_connection_state);
-        button = (Button) view.findViewById(R.id.button);
         setServoButton = (Button) view.findViewById(R.id.setServoButton);
     }
 
@@ -97,59 +82,6 @@ public class OverviewFragment extends Fragment implements DroneConnectionListene
             @Override
             public void onClick(View v) {
                 toggleServo();
-            }
-        });
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MainActivity activity = (MainActivity) getActivity();
-
-                FinalAssignMissionMessage assignMissionMessage = new FinalAssignMissionMessage();
-                MissionDto mission = new MissionDto();
-                ProductDto product = new ProductDto();
-                product.setName("Coca Cola");
-
-                OrderProductDto orderProduct = new OrderProductDto();
-                orderProduct.setAmount(20);
-                orderProduct.setProduct(product);
-
-                mission.setOrderProduct(orderProduct);
-                RouteDto routeDto = new RouteDto();
-
-                ArrayList<Waypoint> wayPoints = new ArrayList<>();
-                Waypoint wp1 = new Waypoint();
-                wp1.setAction(Action.FLY);
-                wp1.setPosition(new Position(47.223343, 8.818480, 20));
-                wayPoints.add(wp1);
-
-                Waypoint wp2 = new Waypoint();
-                wp1.setAction(Action.FLY);
-                wp2.setPosition(new Position(47.222614, 8.817452, 5));
-                wayPoints.add(wp2);
-
-                Waypoint wp6 = new Waypoint();
-                wp6.setAction(Action.FLY);
-                wp6.setPosition(new Position(47.223511, 8.81550, 100));
-                wayPoints.add(wp6);
-
-                Waypoint wp3 = new Waypoint();
-                wp3.setAction(Action.DROP);
-                wp3.setPosition(new Position(47.223510, 8.815553, 50));
-                wayPoints.add(wp3);
-
-                Waypoint wp4 = new Waypoint();
-                wp1.setAction(Action.FLY);
-                wp4.setPosition(new Position(47.224209, 8.818933, 15));
-                wayPoints.add(wp4);
-
-                routeDto.setWayPoints(wayPoints);
-
-                mission.setRoute(routeDto);
-
-                assignMissionMessage.setMission(mission);
-
-                activity.onFinalAssignMissionMessageReceived(assignMissionMessage);
             }
         });
     }
